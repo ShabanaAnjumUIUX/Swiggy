2798138e23832273d08d22184eab7d70c114b3e5
ShabanaAnjumUIUX <shabana.a@techmahindrafoundation.org>
2026-05-11 12:47:50 +0530

    added few things in the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='container'>
       <h1>Welcome to the home page</h1>
+      <h1>Successfully Deployed</h1>
     </div>
   )
 }
